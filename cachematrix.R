ee6a479d8aa440539f3ed8e200602d915c873c5c
Gary Broughton <gbroughton@3degrees.ca>
2015-12-24 09:14:30 -0500

    Assignment Submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# Programming assignment 2
+# 
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix. 
+#             If the inverse has already been calculated (and the matrix has not changed), then the method retrieves the inverse from the cache.
+# 
+# To test, I typed the following into the console:
+# 
+# > mycached <- makeCacheMatrix(matrix(rexp(9), 3)) # generate a random 3 x 3 matrix 
+# > mycached$get() # view the matrix
+# > cacheSolve(mycached) # view the inverse matrix
+# > cacheSolve(mycached) # should see 'returning cached version' message
+# > mycached <- makeCacheMatrix(matrix(rexp(9), 3)) # generate another random 3 x 3 matrix 
+# > cacheSolve(mycached) # matrix has changed, so you should NOT see the 'returning cached version' message
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cachedMatrix <- x 
+  cachedInverse <- NULL
+  
+  get <- function() {
+    return(cachedMatrix)
+  }
+  
+  set <- function(matrix_in) {
+    cachedMatrix <<- matrix_in
+    cachedInverse <<- NULL
+  }
+  
+  getinverse <- function() {
+    return(cachedInverse)
+  }
+  
+  setinverse <- function(matrix_in) {
+    cachedInverse <<- matrix_in 
+  }
+  
+  # define a list of functions that are exposed
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(cacheObj, ...) {
+  ## Check to see if the object has already been cached
+  inverseMatrix <- cacheObj$getinverse()
+  if(!is.null(inverseMatrix)) {
+    # already cached, so simply return value
+    message("returning cached version")
+    return(inverseMatrix)
+  } else {
+    inverse <- solve(cacheObj$get(), ...)  # calculate inverse
+    cacheObj$setinverse(inverse)           # cache value
+    return(inverse)                        # return value
+  }
 }
